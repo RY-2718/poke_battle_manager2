c7d5681d1e80cb77be5dfc4ba19b25b7f3179a75
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-01-29 00:17:10 +0900

    :sparkles: Add: 実数値を表示

--- app/frontend/components/parties/pokeCard.vue
+++ app/frontend/components/parties/pokeCard.vue
@@ -12,16 +12,53 @@
 
 <template>
   <div>
-    <el-card style="width: 100%;">
+    <el-card class="el-card">
       <div slot="header">
         <h5>{{poke.nickname}} - {{poke.poke_name}}</h5>
       </div>
       <el-row>
-        <el-col :xs="12">
+        <el-col :span="12">
           <table>
+            <tr>
+              <th>HP</th>
+              <td>{{ poke.stat.h }}</td>
+            </tr>
+            <tr>
+              <th>こうげき</th>
+              <td>{{ poke.stat.h }}</td>
+            </tr>
+            <tr>
+              <th>ぼうぎょ</th>
+              <td>{{ poke.stat.h }}</td>
+            </tr>
+            <tr>
+              <th>とくこう</th>
+              <td>{{ poke.stat.h }}</td>
+            </tr>
+            <tr>
+              <th>とくぼう</th>
+              <td>{{ poke.stat.h }}</td>
+            </tr>
+            <tr>
+              <th>すばやさ</th>
+              <td>{{ poke.stat.h }}</td>
+            </tr>
           </table>
+        </el-col>
+        <el-col :span="12">
+          hogehoge
         </el-col>
       </el-row>
     </el-card>
   </div>
 </template>
+
+<style scoped>
+  .el-card {
+    margin-bottom: 20px;
+    width: 100%;
+  }
+  th {
+    text-align: right;
+  }
+</style>
